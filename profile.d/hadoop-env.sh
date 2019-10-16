9121e4fc249423decf10e117c7996997ade772d4
tthx <trinhthaihoa@gmail.com>
2019-10-16 09:21:44 +0200

    0.1

--- profile.d/hadoop-env.sh
+++ profile.d/hadoop-env.sh
@@ -1,2 +1,10 @@
+export HADOOP_COMMON_LIB_NATIVE_DIR="${HADOOP_HOME}/lib/native";
+if [[ -n "${LD_LIBRARY_PATH}" ]];
+then
+  export LD_LIBRARY_PATH+=":${HADOOP_COMMON_LIB_NATIVE_DIR}";
+else
+  export LD_LIBRARY_PATH="${HADOOP_COMMON_LIB_NATIVE_DIR}";
+fi
+
 alias start-hs='mapred --daemon start historyserver';
 alias stop-hs='mapred --daemon stop historyserver';
