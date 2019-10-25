cdbb2d1b726888914ebd47ffe26019ae8a077c83
tthx <trinhthaihoa@gmail.com>
2019-10-25 21:39:06 +0200

    0.1

--- compile.sh
+++ compile.sh
@@ -16,17 +16,17 @@ else
   export LD_LIBRARY_PATH="${HADOOP_COMMON_LIB_NATIVE_DIR}";
 fi
 case "${x}" in
-  "testdfsio")
+  testdfsio)
     mvn ${action} -DskipTests
     ;;
-  "hadoop")
+  hadoop)
     mvn ${action} -Pdist,native -DskipTests -Dtar -Dmaven.javadoc.skip=true -Pyarn-ui -Drequire.openssl -Drequire.zstd -Drequire.snappy -Drequire.isal -Disal.prefix=/opt/isa-l -Disal.lib=/opt/isa-l/lib -Dbundle.isal -Dhbase.profile=2.0;
     ;;
-  "hbase")
+  hbase)
     mvn ${action} assembly:single -DskipTests -Dhadoop.profile=3.0 -Dhadoop-three.version=${hadoop_version};
     ;;
-  "spark")
+  spark)
     export MAVEN_OPTS="${JAVA_OPTS} -Xms2g -Xmx2g";
-    ./dev/make-distribution.sh --name spark-2.4.4-bin-without-hadoop-scala-2.12 --tgz --r --pip -T 1C -Dmaven.javadoc.skip=true -DskipTests -Pscala-2.12 -Dscala.version=2.12.10 -Psparkr -Phadoop-2.7 -Dhadoop.version=${hadoop_version} -Phive -Phive-thriftserver -Pmesos -Pyarn -Pkubernetes
+    ./dev/make-distribution.sh --name spark-2.4.4-bin-without-hadoop-scala-2.12 --tgz --r --pip -T 1C -Dmaven.javadoc.skip=true -DskipTests -Pscala-2.12 -Dscala.version=2.12.10 -Psparkr -Phadoop-2.7 -Dhadoop.version=${hadoop_version} -Phadoop-provided -Phive -Phive-thriftserver -Pmesos -Pyarn -Pkubernetes
     ;;
 esac
