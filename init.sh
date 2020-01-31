9bde6642b0ceed866fc34ecf8fac92d87b52b771
Trinh Thai Hoa <thaihoa.trinh@orange.com>
2020-01-31 12:38:13 +0100

    0.0.1

--- init.sh
+++ init.sh
@@ -37,6 +37,6 @@ hdfs dfs -chown -R mapred /home/mapred
 hdfs dfs -chown -R hive /home/hive
 hdfs dfs -chown -R attu7372 /home/attu7372
 hdfs dfs -chown -R hbase /home/hbase
-hdfs dfs -chmod 1777 /home/yarn/log /tmp
+hdfs dfs -chmod 1777 /home/yarn/log /home/hive/warehouse /tmp
 hdfs dfs -chmod g+w /home/hive/warehouse
 hdfs dfs -put /opt/hadoop/share/hadoop/yarn/timelineservice/hadoop-yarn-server-timelineservice-hbase-coprocessor-3.2.1.jar /home/hbase/coprocessor/hadoop-yarn-server-timelineservice.jar
