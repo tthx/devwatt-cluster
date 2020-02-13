79964eb882e263a18e53523d704455dafdfe4b24
Trinh Thai Hoa <thaihoa.trinh@orange.com>
2020-02-13 13:04:23 +0100

    0.0.1

--- init.sh
+++ init.sh
@@ -17,13 +17,14 @@ sudo useradd attu7372 --create-home --groups hadoop --shell /bin/bash
 echo 'attu7372:D@$#H0le99*'|sudo chpasswd
 sudo mkdir -p /mnt/hdfs
 sudo chmod 755 /mnt/hdfs
-sudo mkdir -p /var/hdfs/namesecondary /var/hdfs/data /var/hdfs/edit-1 /var/hdfs/edit-2 /var/hdfs/log /var/hdfs/name-1 /var/hdfs/name-2 /var/hdfs/run /var/yarn/local /var/yarn/log /var/yarn/run /var/mapred/log /var/mapred/run /var/zookeeper/conf /var/zookeeper/log /var/zookeeper/data /var/hbase/log /var/hbase/run /etc/hbase /var/spark/log /var/spark/run /var/hive/log /var/metastore/log
+sudo mkdir -p /var/hdfs/namesecondary /var/hdfs/data /var/hdfs/edit-1 /var/hdfs/edit-2 /var/hdfs/log /var/hdfs/name-1 /var/hdfs/name-2 /var/hdfs/run /var/yarn/local /var/yarn/log /var/yarn/run /var/mapred/log /var/mapred/run /var/zookeeper/conf /var/zookeeper/log /var/zookeeper/data /var/hbase/log /var/hbase/run /etc/hbase /var/spark/log /var/spark/run /var/hive/log /var/hive/tmp /var/metastore/log
 sudo chown -R hdfs:hadoop /var/hdfs /data/hdfs
 sudo chown -R yarn:hadoop /var/yarn
 sudo chown -R mapred:hadoop /var/mapred
 sudo chown -R zookeeper:hadoop /var/zookeeper
 sudo chown -R hbase:hadoop /var/hbase
-sudo chown -R hive:hadoop /var/hive/log /var/metastore/log
+sudo chown -R hive:hadoop /var/hive /var/metastore
+sudo chmod 775 /var/hive/tmp
 sudo chown -R root:root /etc/hadoop /etc/hbase /opt
 sudo chmod -R g-w,o-w /etc/hadoop /etc/hbase /opt
 sudo chmod 644 /opt/hadoop/etc/hadoop/container-executor.cfg
@@ -31,7 +32,7 @@ sudo cp /etc/hadoop/container-executor.cfg /opt/hadoop/etc/hadoop/.
 sudo chown root:hadoop /opt/hadoop/bin/container-executor
 sudo chmod -R 6050 /opt/hadoop/bin/container-executor
 
-hdfs dfs -mkdir -p /home/ubuntu /home/yarn/log /home/mapred /home/hive/warehouse /home/attu7372 /home/hbase/coprocessor /tmp
+hdfs dfs -mkdir -p /home/ubuntu /home/yarn/log /home/mapred /home/hive/warehouse /home/hive/scratch /home/attu7372 /home/hbase/coprocessor /tmp
 hdfs dfs -chown -R ubuntu /home/ubuntu
 hdfs dfs -chown -R yarn /home/yarn
 hdfs dfs -chown -R mapred /home/mapred
@@ -40,4 +41,5 @@ hdfs dfs -chown -R attu7372 /home/attu7372
 hdfs dfs -chown -R hbase /home/hbase
 hdfs dfs -chmod 1777 /home/yarn/log /home/hive/warehouse /tmp
 hdfs dfs -chmod g+w /home/hive/warehouse
+hdfs dfs -chmod 733 /home/hive/scratch
 hdfs dfs -put /opt/hadoop/share/hadoop/yarn/timelineservice/hadoop-yarn-server-timelineservice-hbase-coprocessor-3.2.1.jar /home/hbase/coprocessor/hadoop-yarn-server-timelineservice.jar
