3a9c4ef462a5ab081c94a37ed4881ff8a088ad0a
Trinh Thai Hoa <thaihoa.trinh@orange.com>
2020-03-24 10:32:06 +0100

    0.0.1

--- init.sh
+++ init.sh
@@ -41,7 +41,8 @@ sudo chmod -R g-w,o-w /etc/hadoop /etc/hbase /opt
 sudo cp /etc/hadoop/container-executor.cfg /opt/hadoop/etc/hadoop/.; sudo chmod 644 /opt/hadoop/etc/hadoop/container-executor.cfg; sudo chown root:hadoop /opt/hadoop/bin/container-executor; sudo chmod 6050 /opt/hadoop/bin/container-executor
 # For Hive 3.1.2
 sudo cp /etc/hadoop/container-executor.cfg /opt/hadoop-3.1.2/etc/hadoop/.; sudo chmod 644 /opt/hadoop-3.1.2/etc/hadoop/container-executor.cfg; sudo chown root:hadoop /opt/hadoop-3.1.2/bin/container-executor; sudo chmod 6050 /opt/hadoop-3.1.2/bin/container-executor
-sudo ln -s /usr/share/java/postgresql-jdbc4.jar /opt/hive/lib/.; sudo ln -s /usr/share/java/postgresql-jdbc4.jar /opt/metastore/lib/.; sudo cp ~/src/devwatt-cluster/bin/utils.sh ~/src/devwatt-cluster/bin/metastore_ctl ${METASTORE_HOME}/bin/.; sudo cp ~/src/devwatt-cluster/bin/utils.sh ~/src/devwatt-cluster/bin/hiveserver2_ctl ${HIVE_HOME}/bin/.
+#sudo ln -s /usr/share/java/postgresql-jdbc4.jar /opt/hive/lib/.; sudo ln -s /usr/share/java/postgresql-jdbc4.jar /opt/metastore/lib/.; sudo cp ~/src/devwatt-cluster/bin/utils.sh ~/src/devwatt-cluster/bin/metastore_ctl ${METASTORE_HOME}/bin/.; sudo cp ~/src/devwatt-cluster/bin/utils.sh ~/src/devwatt-cluster/bin/hiveserver2_ctl ${HIVE_HOME}/bin/.
+sudo ln -s /usr/share/java/mysql-connector-java-8.0.19.jar /opt/hive/lib/.; sudo ln -s /usr/share/java/mysql-connector-java-8.0.19.jar /opt/metastore/lib/.; sudo cp ~/src/devwatt-cluster/bin/utils.sh ~/src/devwatt-cluster/bin/metastore_ctl ${METASTORE_HOME}/bin/.; sudo cp ~/src/devwatt-cluster/bin/utils.sh ~/src/devwatt-cluster/bin/hiveserver2_ctl ${HIVE_HOME}/bin/.
 
 hdfs:
 rm -rf /var/hdfs/namesecondary/* /var/hdfs/data/* /data/hdfs/* /mnt/hdfs/* /var/hdfs/edit-1/* /var/hdfs/edit-2/* /var/hdfs/log/* /var/hdfs/name-1/* /var/hdfs/name-2/* /var/yarn/local/*
