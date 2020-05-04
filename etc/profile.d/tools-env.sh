c4b9b318d05c051d9c370d935bb3f19a03ded4bd
Trinh Thai Hoa <thaihoa.trinh@orange.com>
2020-05-04 13:00:23 +0200

    0.0.1

--- etc/profile.d/tools-env.sh
+++ etc/profile.d/tools-env.sh
@@ -34,7 +34,8 @@ alias python='python3';
 alias chrome='google-chrome-stable 2>/dev/null';
 alias firefox='/usr/bin/firefox 2>/dev/null';
 alias sublime='/opt/sublime_text/sublime_text';
-alias docker-clean='docker stop $(docker container ls -a -q) && docker system prune -a -f --volumes';
+alias docker-purge='docker stop $(docker container ls -aq) && docker system prune -a -f --volumes';
+alias docker-clean='docker container stop $(docker container ls -aq) && docker container prune';
 alias python='/usr/bin/python3'
 alias clear-logs='journalctl --vacuum-time=1d';
 alias eclipse='$HOME/eclipse/eclipse 2>/dev/null';
