21e1ff06cd8532040eca404ab30d08709d084459
Trinh Thai Hoa <thaihoa.trinh@orange.com>
2020-06-02 10:25:44 +0200

    0.0.1

--- hql/init.sql
+++ hql/init.sql
@@ -1,7 +1,11 @@
+DROP USER hive;
+DROP USER impala;
+DROP DATABASE metastore;
 CREATE USER hive WITH PASSWORD 'D@$#H0le99*';
 CREATE DATABASE metastore;
 \c metastore;
 \i /opt/hive/scripts/metastore/upgrade/postgres/hive-schema-4.0.0.postgres.sql;
+\i /opt/hive/scripts/metastore/upgrade/postgres/hive-txn-schema-2.3.0.postgres.sql
 \pset tuples_only on
 \o /tmp/grant-privs
 SELECT 'GRANT SELECT,INSERT,UPDATE,DELETE ON "'  || schemaname || '". "' ||tablename ||'" TO hive ;'
@@ -11,13 +15,3 @@ WHERE tableowner = CURRENT_USER and schemaname = 'public';
 \pset tuples_only off
 \i /tmp/grant-privs
 \q
-
-CREATE USER impala WITH PASSWORD 'D@$#H0le99*';
-\pset tuples_only on
-\o /tmp/grant-privs
-SELECT 'GRANT SELECT,INSERT,UPDATE,DELETE ON "'  || schemaname || '". "' ||tablename ||'" TO impala ;'
-FROM pg_tables
-WHERE tableowner = CURRENT_USER and schemaname = 'public';
-\o
-\pset tuples_only off
-\i /tmp/grant-privs
