d26e950709f68b83ffff6cd9e2c9aa64c790e17c
Trinh Thai Hoa <thaihoa.trinh@orange.com>
2020-07-06 13:17:12 +0200

    0.0.1

--- init.sh
+++ init.sh
@@ -52,8 +52,8 @@ done
 rm /tmp/authorized_keys-$(hostname)
 
 git config --global http.proxy http://devwatt-proxy.si.fr.intraorange:8080 \
-&& mkdir -p ~/src \
-&& cd ~/src \
+&& mkdir -p /home/ubuntu/src \
+&& cd /home/ubuntu/src \
 && git clone https://github.com/tthx/devwatt-cluster.git
 
 sudo mkdir -p /var/hdfs/namesecondary /var/hdfs/data /data/hdfs /var/hdfs/edit-1 /var/hdfs/edit-2 /var/hdfs/log /var/hdfs/name-1 /var/hdfs/name-2 /var/hdfs/run /var/yarn/local /var/yarn/log /var/yarn/run /var/mapred/log /var/mapred/run /var/zookeeper/conf /var/zookeeper/log /var/zookeeper/data /var/hbase/log /var/hbase/run /var/spark/log /var/spark/run /var/hive/run /var/hive/log /var/hive/run /var/hive/tmp /var/metastore/run /var/metastore/log /etc/hadoop /etc/hbase /etc/hive /etc/metastore /etc/tez /etc/spark /etc/impala /var/impala/log /var/impala/run /var/impala/tmp \
@@ -112,10 +112,11 @@ sudo ln -sf /usr/share/java/mysql-connector-java-8.0.19.jar /opt/hive/lib/. \
 && sudo ln -sf /usr/share/java/mysql-connector-java-8.0.19.jar /opt/metastore/lib/. \
 && sudo ln -sf /usr/share/java/mysql-connector-java-8.0.19.jar /opt/impala-hive/lib/.
 
-cd ~/src/devwatt-cluster \
+cd /home/ubuntu/src/devwatt-cluster \
 && git pull \
 && sudo cp -r etc/* /etc/. \
 && cd /etc \
+&& sudo mv environment.llap environment \
 && sudo chown -R root:root ./hadoop ./hbase ./hive ./impala ./metastore ./spark ./tez \
 && sudo chmod -R g-w,o-w ./hadoop ./hbase ./hive ./impala ./metastore ./spark ./tez \
 && cd ~/src/devwatt-cluster/bin \
@@ -137,12 +138,12 @@ sudo rm -rf /var/hdfs/namesecondary/* /var/hdfs/data/* /data/hdfs/* /mnt/hdfs/* 
 
 sudo -u hdfs /opt/hadoop/bin/hdfs namenode -format tthx
 
-sudo -u hdfs /opt/hadoop/bin/hdfs dfs -mkdir -p /home/ubuntu /home/yarn/log /home/mapred/mr-history/tmp /home/mapred/mr-history/done /home/hive/warehouse /home/attu7372 /home/hbase/coprocessor /tmp/hive /home/impala/warehouse \
+sudo -u hdfs /opt/hadoop/bin/hdfs dfs -mkdir -p /tmp/hive /home/ubuntu /home/yarn/log /home/mapred/mr-history/tmp /home/mapred/mr-history/done /home/hive/warehouse /home/attu7372 /home/hbase/coprocessor /tmp/hive /home/impala/warehouse \
 && sudo -u hdfs /opt/hadoop/bin/hdfs dfs -chown -R ubuntu /home/ubuntu \
 && sudo -u hdfs /opt/hadoop/bin/hdfs dfs -chown -R yarn /home/yarn \
 && sudo -u hdfs /opt/hadoop/bin/hdfs dfs -chown -R mapred /home/mapred \
 && sudo -u hdfs /opt/hadoop/bin/hdfs dfs -chmod -R 1777 /home/mapred/mr-history \
-&& sudo -u hdfs /opt/hadoop/bin/hdfs dfs -chmod 1770 /home/mapred/mr-history/done \
+&& sudo -u hdfs /opt/hadoop/bin/hdfs dfs -chmod 1777 /home/mapred/mr-history/done \
 && sudo -u hdfs /opt/hadoop/bin/hdfs dfs -chown -R hive /home/hive /tmp/hive \
 && sudo -u hdfs /opt/hadoop/bin/hdfs dfs -chown -R attu7372 /home/attu7372 \
 && sudo -u hdfs /opt/hadoop/bin/hdfs dfs -chown -R hbase /home/hbase \
