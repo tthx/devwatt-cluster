9ad0a92f8360634d42d9981a509972336e81fcb4
yellowred-orange <thaihoa.trinh@orange.com>
2021-03-25 10:33:16 +0100

    0.0.1

--- kubeadm-init.sh
+++ kubeadm-init.sh
@@ -5,6 +5,7 @@ sudo tee /etc/NetworkManager/conf.d/calico.conf <<EOF
 unmanaged-devices=interface-name:cali*;interface-name:tunl*;interface-name:vxlan.calico
 EOF
 
+sudo kubeadm config images pull && \
 sudo kubeadm init \
   --control-plane-endpoint="$(ifconfig ens3|awk '$1~/^inet$/{print $2}')" \
   --apiserver-advertise-address="$(ifconfig ens3|awk '$1~/^inet$/{print $2}')" \
