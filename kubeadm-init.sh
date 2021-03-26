464c14f93f719f8779a964083120b34e49caac0b
Trinh Thai Hoa <thaihoa.trinh@orange.com>
2021-03-26 12:58:19 +0100

    0.0.1

--- kubeadm-init.sh
+++ kubeadm-init.sh
@@ -11,7 +11,7 @@ sudo kubeadm init \
   --apiserver-advertise-address="$(ifconfig ens3|awk '$1~/^inet$/{print $2}')" \
   --pod-network-cidr=192.168.0.0/16 && \
 mkdir -p $HOME/.kube && \
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && \
+sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config && \
 sudo chown $(id -u):$(id -g) $HOME/.kube/config && \
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml && \
 kubectl apply -f https://docs.projectcalico.org/manifests/calicoctl.yaml
