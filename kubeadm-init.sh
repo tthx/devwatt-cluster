2204e86dd1399934f9669fc5f1da55b9e32ac226
Trinh Thai Hoa <thaihoa.trinh@orange.com>
2021-04-16 11:56:11 +0200

    0.0.1

--- kubeadm-init.sh
+++ kubeadm-init.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
-CLUSTER_NAME="ghost-0";
 cd ${HOME}/src/devwatt-cluster && \
+MASTER_IP="$(ifconfig ens3|awk '$1~/^inet$/{print $2}')" && \
+KEY_LENGTH="2048" && \
+CERT_DURATION="365000" && \
+CLIENT_CA="agregator-client-ca" && \
+PROXY_CLIENT="agregator-proxy-client" && \
+DEST_DIR="/etc/kubernetes/pki" && \
+rm -f ${CLIENT_CA}.crt ${CLIENT_CA}.key ${PROXY_CLIENT}.crt ${PROXY_CLIENT}.key && \
+openssl genrsa -out ${CLIENT_CA}.key ${KEY_LENGTH} && \
+openssl req -x509 -new -nodes -key ${CLIENT_CA}.key -subj "/CN=${MASTER_IP}" -days ${CERT_DURATION} -out ${CLIENT_CA}.crt && \
+openssl genrsa -out ${PROXY_CLIENT}.key ${KEY_LENGTH} && \
+openssl req -new -key ${PROXY_CLIENT}.key -subj "/CN=${MASTER_IP}" -out ${PROXY_CLIENT}.csr && \
+openssl x509 -req -in ${PROXY_CLIENT}.csr -CA ${CLIENT_CA}.crt -CAkey ${CLIENT_CA}.key -CAcreateserial -out ${PROXY_CLIENT}.crt -days ${CERT_DURATION} && \
+sudo mkdir -p ${DEST_DIR} && \
+sudo mv ${CLIENT_CA}.crt ${CLIENT_CA}.key ${PROXY_CLIENT}.crt ${PROXY_CLIENT}.key ${DEST_DIR}/.
+sudo chown -R root:root ${DEST_DIR} && \
+sudo chmod 600 ${CLIENT_CA}.key ${PROXY_CLIENT}.key
 sudo kubeadm config images pull && \
+CLUSTER_NAME="ghost-0" && \
 sudo kubeadm init \
   --config="kubeadm-${CLUSTER_NAME}-config.yml" && \
 mkdir -p $HOME/.kube && \
 sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config && \
 sudo chown $(id -u):$(id -g) $HOME/.kube/config && \
-POD_CIDR="172.18.0.0\/16" && \
+POD_CIDR="$(awk '/podSubnet/{gsub("/", "\\/", $2);print $2}' kubeadm-${CLUSTER_NAME}-config.yml)" && \
 curl -s https://docs.projectcalico.org/manifests/calico.yaml | \
   sed -e '/CALICO_IPV4POOL_CIDR/s/\(^.*\)# \(-.*$\)/\1\2/g' \
     -e '/"192.168.0.0\/16"/s/\(^.*\)#.*$/\1  value: "'$POD_CIDR'"/g' \
