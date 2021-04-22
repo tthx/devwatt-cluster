797fcd2231d230031e789419d8adae4978a4b8f1
Trinh Thai Hoa <thaihoa.trinh@orange.com>
2021-04-22 12:35:11 +0200

    0.0.1

--- kubeadm-gen-certs.sh
+++ kubeadm-gen-certs.sh
@@ -9,66 +9,14 @@ CA_DIR="ca";
 CERT_DIR="cert";
 HOST_NAME="$(hostname)";
 HOST_IP="$(ip -f inet -4 address show dev ens3|awk '/inet/{split($2,x,"/");print x[1]}')";
-# We assume that service CIDR is 172.19.0.1/16
-FIRST_SRV_IP="172.19.0.1";
-# Generate root CA
+FIRST_SRV_IP="172.19.0.1"; # We assume that service CIDR is 172.19.0.1/16
+
 rm -rf ./${CA_DIR} ./${CERT_DIR} && \
-mkdir -p ./${CA_DIR} ./${CERT_DIR} && \
-openssl genrsa \
-  -out ./${CA_DIR}/${GHOST_CA}.key \
-  ${CA_KEY_LENGTH} && \
-openssl req \
-  -x509 -new -nodes \
-  -key ./${CA_DIR}/${GHOST_CA}.key \
-  -subj "/CN=${GHOST_CA}" \
-  -addext "keyUsage=critical,digitalSignature,keyEncipherment,keyCertSign" \
-  -days ${CERT_DURATION} \
-  -out ./${CA_DIR}/${GHOST_CA}.crt;
-# Generate k8s, etcd and k8s front proxy CA
-if [[ ${?} -eq 0 ]];
-then
-  for i in ${ETCD_CA} ${K8S_CA} ${K8S_FRONT_PROXY_CA};
-  do
-    openssl genrsa \
-      -out ./${CA_DIR}/${i}.key \
-      ${CA_KEY_LENGTH} && \
-    openssl req \
-      -new -key ./${CA_DIR}/${i}.key \
-      -subj "/CN=${i}" \
-      -addext "keyUsage=critical,digitalSignature,keyEncipherment,keyCertSign" \
-      -out ./${CA_DIR}/${i}.csr && \
-    openssl x509 \
-      -req -in ./${CA_DIR}/${i}.csr \
-      -CA ./${CA_DIR}/${GHOST_CA}.crt \
-      -CAkey ./${CA_DIR}/${GHOST_CA}.key \
-      -CAcreateserial \
-      -CAserial ./${CA_DIR}/${GHOST_CA}.srl \
-      -out ./${CA_DIR}/${i}.crt \
-      -days ${CERT_DURATION};
-    if [[ ${?} -ne 0 ]];
-    then
-      echo "ERROR: Unable to create certificate for ${i}" >&2;
-      exit 1;
-    fi
-  done
-fi
+mkdir -p ./${CA_DIR} ./${CERT_DIR};
 
-cat ./${CA_DIR}/${GHOST_CA}.crt \
-  ./${CA_DIR}/${ETCD_CA}.crt \
-  ./${CA_DIR}/${K8S_CA}.crt \
-  ./${CA_DIR}/${K8S_FRONT_PROXY_CA}.crt > \
-  ./${CA_DIR}/${GHOST_CA}-bundle.crt && \
-openssl verify \
-  -CAfile ./${CA_DIR}/${GHOST_CA}.crt \
-  ./${CA_DIR}/${GHOST_CA}-bundle.crt;
-if [[ ${?} -ne 0 ]];
-then
-  echo "ERROR: Unable to create CA bundle" >&2;
-  exit 1;
-fi
-
-# Generate CA configuration
+# Generate CA configurations
 for i in \
+  ${GHOST_CA} \
   ${ETCD_CA} \
   ${K8S_CA} \
   ${K8S_FRONT_PROXY_CA};
@@ -95,6 +43,59 @@ commonName=supplied
 organizationalUnitName=optional
 EOF
 done
+
+# Generate root CA
+openssl req \
+  -x509 -new -nodes \
+  -newkey rsa:${CA_KEY_LENGTH} \
+  -keyout ./${CA_DIR}/${GHOST_CA}.key \
+  -subj "/CN=${GHOST_CA}" \
+  -addext "keyUsage=critical,digitalSignature,keyEncipherment,keyCertSign" \
+  -addext "basicConstraints=CA:TRUE" \
+  -addext "subjectAltName=DNS:${GHOST_CA}" \
+  -days ${CERT_DURATION} \
+  -out ./${CA_DIR}/${GHOST_CA}.crt;
+
+# Generate k8s, etcd and k8s front proxy CA
+if [[ ${?} -eq 0 ]];
+then
+  for i in ${ETCD_CA} ${K8S_CA} ${K8S_FRONT_PROXY_CA};
+  do
+    openssl req -new -sha256 \
+      -nodes -newkey rsa:${CA_KEY_LENGTH} \
+      -keyout ./${CA_DIR}/${i}.key \
+      -subj "/CN=${i}" \
+      -out ./${CA_DIR}/${i}.csr && \
+    yes yes | openssl ca \
+      -config ./${CA_DIR}/${GHOST_CA}.cfg \
+      -extfile <(echo "
+keyUsage=critical,digitalSignature,keyEncipherment,keyCertSign
+basicConstraints=CA:TRUE
+subjectAltName=DNS:${i}
+") \
+      -out ./${CA_DIR}/${i}.crt \
+      -infiles ./${CA_DIR}/${i}.csr;
+    if [[ ${?} -ne 0 ]];
+    then
+      echo "ERROR: Unable to create certificate for ${i}" >&2;
+      exit 1;
+    fi
+  done
+fi
+
+cat ./${CA_DIR}/${GHOST_CA}.crt \
+  ./${CA_DIR}/${ETCD_CA}.crt \
+  ./${CA_DIR}/${K8S_CA}.crt \
+  ./${CA_DIR}/${K8S_FRONT_PROXY_CA}.crt > \
+  ./${CA_DIR}/${GHOST_CA}-bundle.crt && \
+openssl verify \
+  -CAfile ./${CA_DIR}/${GHOST_CA}.crt \
+  ./${CA_DIR}/${GHOST_CA}-bundle.crt;
+if [[ ${?} -ne 0 ]];
+then
+  echo "ERROR: Unable to create CA bundle" >&2;
+  exit 1;
+fi
 
 # Certificates signed by etcd CA
 KEY_LENGTH="2048";
@@ -238,8 +239,8 @@ then
   sudo cp -f ./${CERT_DIR}/${KUBE_APISERVER_KUBELET_CLIENT}.key ${DEST_DIR}/apiserver-kubelet-client.key && \
   sudo cp -f ./${CA_DIR}/${K8S_FRONT_PROXY_CA}.crt ${DEST_DIR}/front-proxy-ca.crt && \
   sudo cp -f ./${CA_DIR}/${K8S_FRONT_PROXY_CA}.key ${DEST_DIR}/front-proxy-ca.key && \
-  sudo cp -f ./${CERT_DIR}/${FRONT_PROXY_CLIENT}.crt ${DEST_DIR}/front-proxy-client.crt && \
-  sudo cp -f ./${CERT_DIR}/${FRONT_PROXY_CLIENT}.key ${DEST_DIR}/front-proxy-client.key && \
+  sudo cp -f ./${CERT_DIR}/${FRONT_PROXY_CLIENT}.crt ${DEST_DIR}/. && \
+  sudo cp -f ./${CERT_DIR}/${FRONT_PROXY_CLIENT}.key ${DEST_DIR}/. && \
   sudo chown -R root:root ${DEST_DIR} && \
   sudo chmod 600 ${DEST_DIR}/*.key ${DEST_DIR}/etcd/*.key && \
   sudo chmod 644 ${DEST_DIR}/*.crt ${DEST_DIR}/etcd/*.crt
