912052c44cb61c4f234c3b6924edbd7c3a70de2a
Trinh Thai Hoa <thaihoa.trinh@orange.com>
2021-04-26 10:19:45 +0200

    0.0.1

--- kubeadm-init.sh
+++ kubeadm-init.sh
@@ -4,9 +4,7 @@ CLUSTER_NAME="ghost-0";
 POD_CIDR="172.18.0.0/16";
 SRV_CIDR="172.19.0.0/16";
 DOCKER_IMAGE_REPO="dockerfactory-playground.tech.orange";
-kubeadm config images pull --image-repository ${DOCKER_IMAGE_REPO} && \
-sudo kubeadm init \
-  --config=<(echo "
+tee /tmp/${CLUSTER_NAME}.cfg <<EOF
 apiVersion: kubeadm.k8s.io/v1beta2
 kind: ClusterConfiguration
 networking:
@@ -19,24 +17,15 @@ apiServer:
   extraArgs:
     advertise-address: ${HOST_IP}
     requestheader-client-ca-file: /etc/kubernetes/pki/front-proxy-ca.crt
-    requestheader-allowed-names: \"\"
+    requestheader-allowed-names: ""
     requestheader-extra-headers-prefix: X-Remote-Extra-
     requestheader-group-headers: X-Remote-Group
     requestheader-username-headers: X-Remote-User
     proxy-client-cert-file: /etc/kubernetes/pki/front-proxy-client.crt
     proxy-client-key-file: /etc/kubernetes/pki/front-proxy-client.key
-    enable-aggregator-routing: \"true\"") && \
-mkdir -p $HOME/.kube && \
-sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config && \
-sudo chown $(id -u):$(id -g) $HOME/.kube/config && \
-curl -s https://docs.projectcalico.org/manifests/calico.yaml | \
-  sed -e '/CALICO_IPV4POOL_CIDR/s/\(^.*\)# \(-.*$\)/\1\2/g' \
-    -e '/"192.168.0.0\/16"/s/\(^.*\)#.*$/\1  value: "'${POD_CIDR/\//\\\/}'"/g' \
-    -e '/image:\([[:space:]].*\)docker.io\//s/\(^.*\)docker.io\/\(.*$\)/\1\2/g' | \
-  kubectl apply -f - && \
-kubectl apply -f https://docs.projectcalico.org/manifests/calicoctl.yaml && \
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended.yaml && \
-kubectl apply -f <(echo "
+    enable-aggregator-routing: "true"
+EOF
+tee /tmp/dashboard.yml <<EOF
 apiVersion: v1
 kind: ServiceAccount
 metadata:
@@ -54,4 +43,21 @@ roleRef:
 subjects:
 - kind: ServiceAccount
   name: admin-user
-  namespace: kubernetes-dashboard")
+  namespace: kubernetes-dashboard
+EOF
+kubeadm config images pull --image-repository ${DOCKER_IMAGE_REPO} && \
+sudo kubeadm init \
+  --config=/tmp/${CLUSTER_NAME}.cfg && \
+rm -f /tmp/${CLUSTER_NAME}.cfg && \
+mkdir -p $HOME/.kube && \
+sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config && \
+sudo chown $(id -u):$(id -g) $HOME/.kube/config && \
+curl -s https://docs.projectcalico.org/manifests/calico.yaml | \
+  sed -e '/CALICO_IPV4POOL_CIDR/s/\(^.*\)# \(-.*$\)/\1\2/g' \
+    -e '/"192.168.0.0\/16"/s/\(^.*\)#.*$/\1  value: "'${POD_CIDR/\//\\\/}'"/g' \
+    -e '/image:\([[:space:]].*\)docker.io\//s/\(^.*\)docker.io\/\(.*$\)/\1'${DOCKER_IMAGE_REPO}'\/\2/g' | \
+  kubectl apply -f - && \
+kubectl apply -f https://docs.projectcalico.org/manifests/calicoctl.yaml && \
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended.yaml && \
+kubectl apply -f /tmp/dashboard.yml && \
+rm -f /tmp/dashboard.yml
